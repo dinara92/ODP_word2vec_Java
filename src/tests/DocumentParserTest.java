b4625657eb1d44f7fb1f3bf45e08f31a4282d877
dinara-ubuntu <dinara@kaist.ac.kr>
2017-02-22 01:40:51 +0900

    add test to save page content to db

--- src/tests/DocumentParserTest.java
+++ src/tests/DocumentParserTest.java
@@ -1,11 +1,15 @@
 package tests;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
 
 import tfidfDocument.DocumentParser;
 import utils.StringProcessingUtils;
+import webpages_parser.Dmoz_Data;
 
 public class DocumentParserTest {
 	
@@ -21,4 +25,17 @@ public class DocumentParserTest {
 		//assertEquals(docTerms.size(), 8);
 	}
 
+	@Test
+	public void testSavePageContentToDb() {
+		String pageid = "1742389";
+		List<String> lst = new ArrayList<String>();
+		lst.add("1");
+		lst.add("23");
+		try {
+			Dmoz_Data.savePageContentToDB(pageid, lst);
+		} catch (FileNotFoundException | InterruptedException | ExecutionException e) {
+			System.out.println("couldnt execute save");
+			e.printStackTrace();
+		}
+	}
 }
